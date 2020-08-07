d359b76cfa47fc673cbf5cd1d6c68561d22cad61
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-07 18:12:29 -0500

    rename

--- samba/create_user_share.sh
+++ samba/create_user_share.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if [ ! -e "/srv/shares/$USERSHARENAME/$1" ]; then
-    zfs create "srv/shares/$USER_SHARENAME/$1"
+local user_pathend=$(echo "$USERS_SHARENAME" | tr '[:upper:]' '[:lower:]')
+local public_pathend=$(echo "$PUBLIC_SHARENAME" | tr '[:upper:]' '[:lower:]')
+if [ ! -e "/srv/shares/$user_pathend/$1" ]; then
+    zfs create "srv/shares/$user_pathend/$1"
     # chown $1:"shares" /tank/user/$1
     # chmod -R 700 $1 /tank/user/$1
-    ln -s "/srv/shares/$PUBLIC_SHARENAME" "/srv/shares/$USER_SHARENAME/$1/Public"
+
+    ln -s "/srv/shares/$public_pathend" "/srv/shares/$user_pathend/$1/Public"
 fi
--- samba/create_user_tm.sh
+++ samba/create_user_tm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ ! -e "/srv/shares/$TIMEMACHINE_SHARENAME/$1" ]; then
-    mkdir "/srv/shares/$TIMEMACHINE_SHARENAME/$1"
+local pathend=$(echo "$TIMEMACHINE_SHARENAME" | tr '[:upper:]' '[:lower:]')
+if [ ! -e "/srv/shares/$pathend/$1" ]; then
+    mkdir "/srv/shares/$pathend/$1"
     # chown " /tank/time_machine/$1
     # chmod -R 700 $1 /tank/time_machine/$1
 fi
