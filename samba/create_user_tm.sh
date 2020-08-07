2aec3b4b6403a4addaebd6ec41f75342becc9139
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-07 18:19:56 -0500

    rename

--- samba/create_user_tm.sh
+++ samba/create_user_tm.sh
@@ -3,6 +3,6 @@
 pathcomp=$(echo "$TIMEMACHINE_SHARENAME" | tr '[:upper:]' '[:lower:]')
 if [ ! -e "/srv/shares/$pathcomp/$1" ]; then
     mkdir "/srv/shares/$pathcomp/$1"
-    # chown " /tank/time_machine/$1
+    chown -R smbuser:smb "/srv/shares/$pathcomp/$1"
     # chmod -R 700 $1 /tank/time_machine/$1
 fi
