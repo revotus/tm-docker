dba7ea3c9fcdd555d5c331ada66b06338d00ee89
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-07 17:50:00 -0500

    add create suer script

--- samba/create_user_share.sh
+++ samba/create_user_share.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -e /tank/user/$1 ]; then
-    zfs create tank/user/$1
-    chown $1:"shares" /tank/user/$1
-    chmod -R 700 $1 /tank/user/$1
-    ln -s /tank/public /tank/user/$1/Public
+if [ ! -e "/tank/$USERSHARENAME/$1" ]; then
+    zfs create "tank/$USER_SHARENAME/$1"
+    # chown $1:"shares" /tank/user/$1
+    # chmod -R 700 $1 /tank/user/$1
+    ln -s "/tank/$PUBLIC_SHARENAME" "/tank/$USER_SHARENAME/$1/Public"
 fi
