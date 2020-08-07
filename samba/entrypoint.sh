0c2a5c2641a41d20d9b5316b229101144b07e097
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-07 17:45:28 -0500

    add create tm script

--- samba/entrypoint.sh
+++ samba/entrypoint.sh
@@ -70,7 +70,7 @@ add_timemachine () {
     conf-utils setvar -y -q -s "$TIMEMACHINE_SHARENAME" -n "valid users" -a "$TIMEMACHINE_VALIDUSERS" "$SMBCONF"
     conf-utils setvar -y -q -s "$TIMEMACHINE_SHARENAME" -n "writeable" -a "yes" "$SMBCONF"
     conf-utils setvar -y -q -s "$TIMEMACHINE_SHARENAME" -n "fruit:time machine" -a "yes" "$SMBCONF"
-    conf-utils setvar -y -s "$TIMEMACHINE_SHARENAME" -n "root preexec" -a "/usr/bin/create_user_time_machine.sh %U" "$SMBCONF"
+    conf-utils setvar -y -s "$TIMEMACHINE_SHARENAME" -n "root preexec" -a "/usr/bin/create_user_time_machine.sh %U" -f "pretty" "$SMBCONF"
 }
 
 while getopts ":u:PUTn:" opt; do
