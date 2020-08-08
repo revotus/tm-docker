5ac37c08d21c1c0fa9d15c88b9c6a915302b5c1b
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-08 03:07:47 -0500

    tighten up cretae user share

--- samba/create_user_share.sh
+++ samba/create_user_share.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+source ./.env
 username="$1"
 
 users_dirname=$(echo "$USERS_SHARENAME" | tr '[:upper:]' '[:lower:]')
 public_dirname=$(echo "$PUBLIC_SHARENAME" | tr '[:upper:]' '[:lower:]')
-smbvol_host_dirname=${SMBVOL_HOST#/}
 
-if [ ! -d "$SMBVOL_HOST/$users_dirname/$username" ]; then
-    zfs create "$smbvol_host_dirname/$users_dirname/$username"
-    chown -R $SMBUID:$SMBGID "$SMBVOL_HOST/$users_dirname/$username"
+smb_path="$SMBVOL_PATH_HOST/$users_dirname/$username"
+zfs_path="${SMBVOL_PATH_HOST#/}/$users_dirname/$username"
+public_path="$SMBVOL_PATH_HOST/$public_dirname"
+
+if [ ! -d  "$smb_path" ]; then
+    zfs create "$zfs_path"
+    chown -R $SMBUID:$SMBGID "$smb_path"
     # chmod -R 700 $1 /tank/user/$1
 
-    ln -s "$SMBVOL_HOST/$public_dirname" "$SMBVOL_HOST/$users_dirname/$username/$PUBLIC_SHARENAME"
+    ln -s  "$public_path" "$smb_path/$PUBLIC_SHARENAME"
 fi
