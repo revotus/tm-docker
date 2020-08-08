8265c3e136e31aa3f9cca8c32bb9a102762a3c41
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-08 03:01:32 -0500

    stupid

--- samba/create_user_tm.sh
+++ samba/create_user_tm.sh
@@ -5,7 +5,7 @@ username="$1"
 tm_dirname=$(echo "$TIMEMACHINE_SHARENAME" | tr '[:upper:]' '[:lower:]')
 tm_path="$SMBVOL_PATH_CONTAINER/$tm_dirname/$username"
 
-if [ ! -d  "$tm_path"]; then
+if [ ! -d  "$tm_path" ]; then
     mkdir "$tm_path"
     chown -R $SMBUID:$SMBGID "$tm_path"
     # chmod -R 700 $1 /tank/time_machine/$1
