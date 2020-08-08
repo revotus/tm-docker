9fe0f3c846fe1d8d13ad3be23c8b0f8e2ce12a73
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-08 02:58:06 -0500

    change vars in create tm

--- samba/create_user_tm.sh
+++ samba/create_user_tm.sh
@@ -3,8 +3,10 @@
 username="$1"
 
 tm_dirname=$(echo "$TIMEMACHINE_SHARENAME" | tr '[:upper:]' '[:lower:]')
-if [ ! -d "$SMBVOL_CONTAINER/$tm_dirname/$username" ]; then
-    mkdir "$SMBVOL_CONTAINER/$tm_dirname/$username"
-    chown -R $SMBUID:$SMBGID "$SMBVOL_CONTAINER/$tm_dirname/$username"
+tm_path="$SMBVOL_PATH_CONTAINER/$tm_dirname/$username"
+
+if [ ! -d  "$tm_path"]; then
+    mkdir "$tm_path"
+    chown -R $SMBUID:$SMBGID "$tm_path"
     # chmod -R 700 $1 /tank/time_machine/$1
 fi
