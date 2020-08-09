e1a8e81d8697becacf644b4789f007209386f876
Matthew Otus Benak <matt.benak@gmail.com>
2020-08-09 00:44:43 -0500

    renames

--- samba/create_user_share.sh
+++ samba/create_user_share.sh
@@ -12,7 +12,7 @@ public_path="$SMBVOL_PATH_HOST/$public_dirname"
 
 if [ ! -d  "$smb_path" ]; then
     zfs create "$zfs_path"
-    chown -R $username:$SMB_GID "$smb_path"
+    chown -R $SMB_UID:$SMB_GID "$smb_path"
     chmod -R 700 "$smb_path"
 
     ln -s  "$public_path" "$smb_path/$PUBLIC_SHARENAME"
